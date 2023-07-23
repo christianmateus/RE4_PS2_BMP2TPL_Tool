fd3920eda7a3afac7a934669c16ce85e41cdd394
Christian Mateus Ferreira Nunes <christianmateus016@gmail.com>
2023-07-22 22:58:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using TPLUtil;
+
+namespace RE4_PS2_TPL_BMP_Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[1] == "to-bmp")
+            {
+                ConverterBMP converterBMP = new ConverterBMP();
+                converterBMP.TPLtoBMP(args[0]);
+            }
+            else
+            {
+                ConverterBMP converterBMP = new ConverterBMP();
+                converterBMP.BMPtoTPL(args[0]);
+            }
+        }
+    }
+}
